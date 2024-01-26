3c04c92d00c57b4beb16d0b347cefd842726e249
cadillac-cyrus <myredevesuper@gmail.com>
2024-01-26 17:15:15 +0000

    Commit

--- cybix-web/src/pages/index.js
+++ cybix-web/src/pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 import Head from "next/head";
+import { createParser } from "eventsource-parser";
 
 const SYSTEM_MESSAGE =
   "You are Cybix,a helpful and versatile AI created by Coxwell using state of the art machine learning models and API's";
@@ -18,105 +19,139 @@ export default function Home() {
 
   const API_URL = "https://chat-gpt26.p.rapidapi.com/";
 
-  async function sendRequest() {
-    // update the message history
-    const newMessage = {role: "user", content: userMessage};
-    const newMessages = [
+  const sendRequest = async () => {
+    const updatedMessages = [
       ...messages,
-      newMessage
-    ]
+      {
+        role: "user",
+        content: userMessage,
+      },
+    ];
 
-    setMessages(newMessages);
+    setMessages(updatedMessages);
     setUserMessage("");
 
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: "Bearer " + apiKey,
+          "X-RapidAPI-Key": apiKey,
+          "X-RapidAPI-Host": "chat-gpt26.p.rapidapi.com",
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+          stream: true,
+        }),
+      });
 
+      const reader = response.body.getReader();
 
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Authorization: "Bearer " + apiKey,
-        "X-RapidAPI-Key": apiKey,
-        "X-RapidAPI-Host": "chat-gpt26.p.rapidapi.com",
-      },
-      body: JSON.stringify({
-        messages: newMessages,
-      }),
-    });
+      let newMessage = "";
+      const parser = createParser((event) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            return;
+          }
+          const json = JSON.parse(event.data);
+          const content = json.choices[0].delta.content;
 
-    const responseJson = await response.json();
+          if (!content) {
+            return;
+          }
 
-    const newBotMessage = responseJson.choices[0].message;
+          newMessage += content;
 
-    const newMessages2 = [...newMessages, newBotMessage];
+          const updatedMessages2 = [
+            ...updatedMessages,
+            { role: "assistant", content: newMessage },
+          ];
 
-    setMessages(newMessages2);
+          setMessages(updatedMessages2);
+        } else {
+          return "";
+        }
+      });
+
+      // eslint-disable-next-line
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder().decode(value);
+        parser.feed(text);
+      }
+    } catch (error) {
+      console.error("error");
+      window.alert("Error:" + error.message);
+    }
 
     // setBotMessage(responseJson.result);
 
     // console.log("botMessage", botMessage);
 
-    console.log("responseJson", responseJson);
-  }
+    // console.log("responseJson", responseJson);
+  };
 
   return (
-    <><Head>
-      <title>
-        Cybix - Your Friendly neighborhood AI
-      </title>
-    </Head>
-    <div className="flex flex-col h-screen">
-      {/* Navbar */}
-      <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
-        <div className="text-xl font-bold">Cybix</div>
+    <>
+      <Head>
+        <title>Cybix - Your Friendly neighborhood AI</title>
+      </Head>
+      <div className="flex flex-col h-screen">
+        {/* Navbar */}
+        <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+          <div className="text-xl font-bold">Cybix</div>
+          <div>
+            <input
+              type="password"
+              className="border p-1 rounded"
+              onChange={(e) => setApiKey(e.target.value)}
+              value={apiKey}
+              placeholder="Paste Api Key Here"
+            ></input>
+          </div>
+        </nav>
+
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll">
+          <div className="w-full max-w-screen-md mx-auto px-4">
+            {messages
+              .filter((message) => message.role !== "system")
+              .map((message, idx) => (
+                <div key={idx} className="my-3">
+                  <div className="font-bold">
+                    {message.role === "user" ? "You" : "Cybix"}
+                  </div>
+                  <div className="text-lg prose">
+                    <ReactMarkdown>{message.content}</ReactMarkdown>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Message Input Box */}
         <div>
-          <input
-            type="password"
-            className="border p-1 rounded"
-            onChange={(e) => setApiKey(e.target.value)}
-            value={apiKey}
-            placeholder="Paste Api Key Here"
-          ></input>
+          <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
+            <textarea
+              value={userMessage}
+              onChange={(e) => setUserMessage(e.target.value)}
+              className="border text-lg rounded-md p-1 flex-1 "
+              rows={1}
+            />
+            <button
+              onClick={sendRequest}
+              className="border rounded-md bg-blue-500 hover:bg-blue-600 text-white px-4 ml-2"
+            >
+              Send
+            </button>
+          </div>
         </div>
-      </nav>
 
-      {/* Message History */}
-      <div className="flex-1 overflow-y-scroll">
-        <div className="w-full max-w-screen-md mx-auto px-4">
-          {messages.filter(message => message.role !== "system").map((message, idx) => (
-            <div key={idx} className="my-3">
-              <div className="font-bold">
-                {message.role === "user" ? "You" : "Cybix"}
-              </div>
-              <div className="text-lg prose">
-                <ReactMarkdown>
-                  {message.content}
-                </ReactMarkdown>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Message Input Box */}
-      <div>
-        <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
-          <textarea
-            value={userMessage}
-            onChange={(e) => setUserMessage(e.target.value)}
-            className="border text-lg rounded-md p-1 flex-1 "
-            rows={1}
-          />
-          <button
-            onClick={sendRequest}
-            className="border rounded-md bg-blue-500 hover:bg-blue-600 text-white px-4 ml-2"
-          >
-            Send
-          </button>
-        </div>
-      </div>
-
-      {/* Test Button
+        {/* Test Button
       <div className="p-4">
         <button
           onClick={sendRequest}
@@ -126,7 +161,7 @@ export default function Home() {
         </button>
         <div className="mt-4 text-lg">{botMessage}</div>
       </div> */}
-    </div>
+      </div>
     </>
   );
 }
