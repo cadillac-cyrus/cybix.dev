007c5dc61e2145b13cebc70828f084f634640f01
cadillac-cyrus <myredevesuper@gmail.com>
2024-01-30 14:21:32 -0800

    Added Login Page

--- cybix-web/src/pages/index.js
+++ cybix-web/src/pages/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Head from "next/head";
 import ReactMarkdown from 'react-markdown'
 import { createParser } from "eventsource-parser";
+import Navbar from "@/Components/Navbar";
 // import { useEffect } from "react";
 
 export default function Home() {
@@ -95,20 +96,7 @@ export default function Home() {
       </Head>
       <div className="flex flex-col h-screen">
         {/* Navbar */}
-        <nav className="bg-white shadow w-full">
-          <div className="px-4 h-14 flex justify-between items-center">
-            <div className="text-xl font-bold">Cybix</div>
-            <div>
-              <input
-                type="password"
-                className="border rounded p-1"
-                placeholder="Enter API key.."
-                value={apiKey}
-                onChange={(e) => setApiKey(e.target.value)}
-              />
-            </div>
-          </div>
-        </nav>
+        <Navbar />
 
         {/* Message History */}
         <div className="flex-1 overflow-y-scroll">
